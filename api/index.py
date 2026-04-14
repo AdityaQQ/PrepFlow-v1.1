debc38c88ff0888d9c699106b7c71f9f6831bc20
Yves <qcxwae@gmail.com>
2026-04-14 13:23:02 +0530

    fix python path final

--- api/index.py
+++ api/index.py
@@ -1,9 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+# Add project root to Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from prepflow1.app import app
-
-# Vercel needs this
-app = app
